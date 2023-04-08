3c0e19daf25df4a744a1bc25bb2caa5a78c337dd
Orbis1 <infectores@gmail.com>
2023-04-08 15:47:13 +0000

    Orbis1 published a site update

---
+++ 4/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const isCorrectPassword = (password) => {
+    const chars = password.replace(/\d/gm, '');
+    const numbers = password.split(/[а-яА-Я]/gm);
+    const sum = numbers
+        .map(n => parseInt(n, 10))
+        .filter(n => !Number.isNaN(n))
+        .reduce((a, b) => a + b);
+    console.log({ password, chars, numbers, sum });
+    return chars === 'БММММ' && sum === 873;
+};
+isCorrectPassword('Б1М2М3М4М');
